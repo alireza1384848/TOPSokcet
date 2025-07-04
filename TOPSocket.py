3015aee7796cea261abf420d040ff8d09a09b8ac
Alireza <alireza.rohollahi031@gmail.com>
2025-07-04 19:47:08 +0330

    add connet method and complete accept initialize TOPConnection class

--- TOPSocket.py
+++ TOPSocket.py
@@ -1,7 +1,9 @@
+import datetime
 import queue
 import socket
 import threading
 import random
+from logging import exception
 
 from TOPConnection import TOPConnection as Connection
 from TOUPacket import TOUPacket as packet
@@ -14,11 +16,15 @@ class TOPSocket:
     remote_port :int = 0
     udp_socket :socket.socket = None
     accept_queue :queue.Queue = queue.Queue()
-    active_connections :list = dict[(str,int),Connection]
+    server_port :int = 0
     ack_dict = dict[(str,int),int]
     is_closed :bool = False
     is_server :bool = False
+    is_client :bool = False
     mss : int  =1024
+
+    server_ip :str = ''
+    server_port :int = 0
 
 
     def __init__(self) -> None:
@@ -44,14 +50,70 @@ class TOPSocket:
        else:
            raise Exception('udp socket not initialized')
 
+
     def listen(self,accept_queue_size = 3):
         """
         listen on a socket to make a connection
 
         :param accept_queue_size: maximum number of  connections want to accept
         """
-        self.is_server = True
-        self.accept_queue = queue.Queue(accept_queue_size)
+        if  not self.is_client:
+            self.is_server = True
+            self.accept_queue = queue.Queue(accept_queue_size)
+            threading.Thread(target=self.buffering_thread(),args=()).start()
+
+    def accept(self):
+            """
+            accept connection and make a TOP connection
+            :returns: TOPConnection , ipaddr and port
+            """
+            addr = self.accept_queue.get()
+            conn = Connection(self.udp_socket
+                              , addr[0]
+                              , addr[1]
+                              , self.src_ip
+                              , self.src_port)
+            return conn,addr
+
+    def connect(self,ipaddr,port,window_size=5000,mss=1024):
+        if self.is_server :
+            raise exception("This is a client_method but your socket is server.")
+        self.is_client = True
+        randseq = self.generate_seq()
+        syn_packet =packet(self.src_port,
+                           port,
+                            randseq,
+                            0,
+                            {'SYN':True,'ACK':False
+                                ,'RST':False,'FIN':False}
+                                ,window_size
+                                ,mss)
+        self.udp_socket.sendto(syn_packet.to_bytes(),(ipaddr,port))
+        try :
+            self.udp_socket.settimeout(5)
+            recv_byte =self.udp_socket.recvfrom(mss)
+            pack = packet.from_bytes(recv_byte)
+            #todo : check this condition
+            if pack.acknum == randseq+1 :
+                ack_packet = packet(self.src_port,
+                                    port,
+                                    randseq+1,
+                                    pack.seqnum,
+                                    {'SYN': False, 'ACK': True
+                                        , 'RST': False, 'FIN': False}
+                                    , window_size
+                                    , mss)
+
+            self.udp_socket.sendto(ack_packet.to_bytes(),(ipaddr,port))
+            self.server_ip = ipaddr
+            self.server_port = port
+        except socket.timeout as e :
+            print(f"[{datetime.now()}] Connection timed out Server didn't response.")
+            raise exception("Socket timed out.")
+        except exception as e :
+            print(f"[{datetime.now()}] Error occured {e}.")
+            self.is_client = False
+            raise exception(f"an undefined error occured. {e}")
 
     def buffering_thread(self):
         """
@@ -65,9 +127,12 @@ class TOPSocket:
 
         Identify invalid packets or packets from unknown connections, and ignore or respond appropriately (e.g., RST packet) to them.
          """
+
         while not self.is_closed:
+            self.udp_socket.settimeout(5)
             recv_bytes, addr= self.udp_socket.recvfrom(1024)
             pack = None
+            print(f"[{datetime.now()}] Buffering thread started on {self.src_ip}:{self.src_port} ")
             try:
                 pack = packet.from_bytes(recv_bytes)
                 randseq = self.generate_seq()
@@ -90,8 +155,6 @@ class TOPSocket:
                             self.accept_queue.put(addr)
                 # send packet to appropriate connection.
                 # Todo : compelete this and connection
-
-
 
 
             except Exception as e:
--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-len(int)
+import queue
+
+bu = queue.Queue()
+bu.put(1)
+bu.put(2)
+
+
+print (bu.qsize())
+
+
+buffer = bytearray()
+
+buffer += b'10001'
+buffer += b'0001'
+buffer += b'10000'
+
+
+print(buffer)
